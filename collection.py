94dc6790c6639f1378437043793d766bf7ea8abc
David Mandelberg <david@mandelberg.org>
2016-11-04 22:08:03 -0400

    Start on configuration for CoHydra.
    This commit uses temporary paths for testing.

---
+++ collection.py
@@ -0,0 +1,194 @@
+import logging
+import mimetypes
+import os
+import subprocess
+
+import cohydra.profile
+
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+
+music_master = cohydra.profile.RootProfile(
+  #top_dir='/home/dseomn/Music/master',
+  top_dir='/tmp/tmp.RtgBlV2sVk/master',
+  )
+
+
+def music_default_select_cb(profile, dir, contents):
+  dir_split = os.path.normpath(dir).split(os.sep)
+  if dir_split == ['']:
+    depth = 0
+  else:
+    depth = len(dir_split)
+
+  if depth < 2:
+    return contents
+  elif depth > 2:
+    raise RuntimeError('this should not be possible')
+
+  keep = []
+  images = []
+
+  for entry in contents:
+    if entry.is_dir():
+      # Skip it.
+      continue
+
+    mime, encoding = mimetypes.guess_type(entry.path)
+    if mime is None:
+      if entry.name.endswith('.toc'):
+        mime = 'application/x-cdrdao-toc'
+      else:
+        profile.log(
+          logging.ERROR,
+          'Unknown mimetype for %r',
+          entry.path,
+          )
+        keep.append(entry)
+        continue
+
+    mime_major, __discard, __discard = mime.partition('/')
+
+    if mime_major in (
+        'audio',
+        ):
+      keep.append(entry)
+    elif mime_major in (
+        'application',
+        'text',
+        'video',
+        ):
+      # Skip it.
+      continue
+    elif mime_major in (
+        'image',
+        ):
+      images.append(entry)
+    else:
+      profile.log(
+        logging.ERROR,
+        'Unsure what to do with %r, %r',
+        mime,
+        entry.path,
+        )
+      keep.append(entry)
+
+  for prefix in (
+      'front',
+      'cover',
+      'front+spine',
+      'medium',
+      'cd',
+      'front+spine+back',
+      'back',
+      'back+spine',
+      #'spine',
+      #'unknown',
+      #'other',
+      None,
+      ):
+    for suffix in (
+        'png',
+        'tif',
+        'jpg',
+        'gif',
+        None,
+        ):
+      for image in images:
+        if prefix is not None and suffix is not None:
+          if image.name == prefix + '.' + suffix:
+            return keep + [image]
+        elif prefix is not None:
+          if image.name.startswith(prefix + '.'):
+            self.log(
+              logging.INFO,
+              'Using sub-optimal image %r',
+              image.path,
+              )
+            return keep + [image]
+        elif suffix is not None:
+          if image.name.endswith('.' + suffix):
+            self.log(
+              logging.INFO,
+              'Using sub-optimal image %r',
+              image.path,
+              )
+            return keep + [image]
+        else:
+          self.log(
+            logging.WARNING,
+            'Using arbitrary image %r',
+            image.path,
+            )
+          return keep + [image]
+
+  return keep
+
+music_default = cohydra.profile.FilterProfile(
+  #top_dir='/home/dseomn/Music/profiles/default',
+  top_dir='/tmp/tmp.RtgBlV2sVk/default',
+  parent=music_master,
+  select_cb=music_default_select_cb,
+  )
+
+
+def music_large_select_cb(profile, src_relpath):
+  if src_relpath.endswith('.flac'):
+    return src_relpath + '.ogg'
+  else:
+    return None
+
+def music_large_convert_cb(profile, src, dst):
+  subprocess.run(
+    [
+      'oggenc',
+      '-Q',
+      '-q', '8',
+      '-o', dst,
+      '--', src,
+      ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    check=True,
+    )
+
+music_large = cohydra.profile.ConvertProfile(
+  #top_dir='/home/dseomn/Music/profiles/large',
+  top_dir='/tmp/tmp.RtgBlV2sVk/large',
+  parent=music_default,
+  select_cb=music_large_select_cb,
+  convert_cb=music_large_convert_cb,
+  )
+
+
+def music_videos_select_cb(profile, dir, contents):
+  keep = []
+
+  for entry in contents:
+    if entry.is_dir():
+      keep.append(entry)
+      continue
+
+    mime, encoding = mimetypes.guess_type(entry.path)
+    if mime is None:
+      continue
+
+    if mime.startswith('video/') or mime in (
+        'application/x-iso9660-image',
+        ):
+      keep.append(entry)
+
+  return keep
+
+music_videos = cohydra.profile.FilterProfile(
+  #top_dir='/home/dseomn/Videos/[music]',
+  top_dir='/tmp/tmp.RtgBlV2sVk/videos',
+  parent=music_master,
+  select_cb=music_videos_select_cb,
+  )
+
+
+if __name__ == '__main__':
+#  music_master.print_all()
+  music_master.generate_all()
